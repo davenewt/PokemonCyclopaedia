80e562bfbbefbae683aade3dd632c39b13bb5902
David Stroud <david@dwstroud.com>
2016-04-15 18:35:32 +0100

    Implement search functionality

--- PokemonCyclopaedia/ViewController.swift
+++ PokemonCyclopaedia/ViewController.swift
@@ -9,14 +9,17 @@
 import UIKit
 import AVFoundation
 
-class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UISearchBarDelegate {
                                         // implement the protocol, each has its methods. Ctrl-click to get more info on them.
     
     @IBOutlet weak var collection: UICollectionView!
     
-    var pokemon = [Pokemon]() // empty pokemon array
+    @IBOutlet weak var searchBar: UISearchBar!
     
+    var pokemon = [Pokemon]() // empty pokemon array
+    var filteredPokemon = [Pokemon]() // for searching
     var musicPlayer: AVAudioPlayer! // remember to import AVFoundation above first.
+    var inSearchMode = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +28,8 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         
         collection.delegate = self
         collection.dataSource = self
+        searchBar.delegate = self
+        searchBar.returnKeyType = UIReturnKeyType.Done // change the word on the return key when searching
         
         parsePokemonCSV()
         initAudio()
@@ -68,7 +73,14 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         
         if let cell = collectionView.dequeueReusableCellWithReuseIdentifier("PokeCell", forIndexPath: indexPath) as? PokeCell {
             
-            let poke = pokemon[indexPath.row]
+            let poke: Pokemon!
+            
+            if inSearchMode {
+                poke = filteredPokemon[indexPath.row]
+            } else {
+                poke = pokemon[indexPath.row]
+            }
+            
             cell.configureCell(poke)
             
             return cell // if able to grab a cell of this type, configure it and then return it
@@ -86,7 +98,10 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     }
     
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 718
+        if inSearchMode {
+            return filteredPokemon.count
+        }
+        return pokemon.count
     }
     
     func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
@@ -107,6 +122,27 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
             sender.alpha = 1.0
         }
         
+    }
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        view.endEditing(true) // hide the keyboard when user presses the search button in the search bar
+    }
+    
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text == nil || searchBar.text == "" {
+            // todo: when you type in the field then delete all the text, this assumes you're done searching and exits search mode. I want to be able to see if the cursor is still in the search bar, and only if someone clicks off it, it will end searching.
+            inSearchMode = false
+            view.endEditing(true) // close the keyboard
+            collection.reloadData() // reload the data! Must remember to do this!
+        } else {
+            inSearchMode = true
+            let lower = searchBar.text!.lowercaseString // convert anything typed into lowercase, just in case someone types in caps
+            filteredPokemon = pokemon.filter({$0.name.rangeOfString(lower) != nil})
+            // $0 is a placeholder for the element being looked at
+            // this takes the elements from the pokemon array, sees if each one contains the string being searched for, and if it is, adds it to the filteredPokemon array automatically!
+            // explained at 9mins in at https://www.udemy.com/ios9-swift/learn/v4/t/lecture/3450388
+            collection.reloadData() // reload the data! Must remember to do this!
+        }
     }
 
 
